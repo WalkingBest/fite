29dec8c6520c7dff53129150828cf01d1af99d8a
Noe Ruiz <jnrg_ruiz@hotmail.com>
2019-04-15 15:16:49 -0700

    temp

--- js/scripts.js
+++ js/scripts.js
@@ -63,3 +63,25 @@ let divlooser = document.createElement("P");
 divlooser.innerHTML = (perdedor + " ha perdido");
 divlooser.setAttribute("class", "perdedor");
 document.getElementById("perdedor").appendChild(divlooser);
+
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYoutubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "560",
+    width: "315",
+    videoId: "cPCLFtxpadE",
+    events: {
+      "onReady": onPlayerReady,
+      "onStateChange": onPlayerStateChange
+    }
+  });
+}
+function onPlayerReady(event){
+  event.target.playVideo();
+}
